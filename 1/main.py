4020880cab6ae2f4591ab90bc641526ea739f695
hwalk1 <haydenjwalker1@gmail.com>
2024-12-04 16:18:15 +1100

    Readability

--- 1/main.py
+++ 1/main.py
@@ -1,24 +1,18 @@
-with open('data.txt', 'r') as file:
-   lines = file.readlines()
 
 a = []
 b = []
-total = 0
 
-for line in lines:
-   s = line
-   aval, bval = map(int, s.split('   '))
-   a.append(aval)
-   b.append(bval)
+with open('data.txt', 'r') as file:
+   for line in file:
+      s = line
+      aval, bval = map(int, s.split('   '))
+      a.append(aval)
+      b.append(bval)
 
 a.sort()
 b.sort()
 
+total = 0
 for idx, x in enumerate(a):
-
    total += (abs(a[idx] - b[idx]))
-
-print(total)
-
-
 
